6f0797f691a7f7a525614e24ee7614731fd1349d
Yiding He <yiding.he@gmail.com>
2018-02-20 13:26:47 +0800

    以 zip 格式保存文件，以节省空间。读取时兼容之前的纯文本格式。

--- src/main/java/com/hyd/pass/model/PasswordLib.java
+++ src/main/java/com/hyd/pass/model/PasswordLib.java
@@ -4,11 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.hyd.pass.App;
 import com.hyd.pass.utils.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import static com.hyd.pass.utils.AESUtils.encode128;
 import static com.hyd.pass.utils.Bytes.md5;
@@ -45,8 +52,8 @@ public class PasswordLib {
             String masterPasswordValidator;
             JSONObject jsonObject;
             try {
-                String json = FileUtils.read(saveFile);
-                jsonObject = JSON.parseObject(json);
+                String content = readContent(saveFile);
+                jsonObject = JSON.parseObject(content);
                 masterPasswordValidator = jsonObject.getString("masterPasswordValidator");
             } catch (IOException e) {
                 throw new PasswordLibException("无法打开文件", e);
@@ -128,10 +135,38 @@ public class PasswordLib {
         data.put("rootCategory", rootCategory);
 
         try {
-            FileUtils.write(this.saveFile, JSON.toJSONString(data, true), charset);
+            try(ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(saveFile))) {
+                saveContent(data, zos);
+            }
             setChanged(false);
         } catch (IOException e) {
             throw new PasswordLibException(e);
+        }
+    }
+
+    private void saveContent(Map<String, Object> data, ZipOutputStream zos) throws IOException {
+        String content = JSON.toJSONString(data, true);
+
+        zos.putNextEntry(new ZipEntry("content.json"));
+        zos.write(content.getBytes(charset));
+        zos.closeEntry();
+    }
+
+    private String readContent(File saveFile) throws IOException {
+        try {
+            try (ZipFile f = new ZipFile(saveFile)) {
+                ZipEntry entry = f.getEntry("content.json");
+                if (entry != null) {
+                    try (InputStream is = f.getInputStream(entry)) {
+                        return IOUtils.toString(is, charset);
+                    }
+                } else {
+                    throw new IOException("文件内容不存在");
+                }
+            }
+        } catch (ZipException e) {
+            // 读取时兼容最初的纯文本格式，但保存时以 zip 格式保存
+            return FileUtils.read(saveFile);
         }
     }
 

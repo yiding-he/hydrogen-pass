1c986573d8f9ecbcdf11c1cfd89b06591338523d
Yiding He <yiding.he@gmail.com>
2018-07-13 19:23:08 +0800

    修改右键菜单

--- src/main/java/com/hyd/pass/fx/EntryTableRow.java
+++ src/main/java/com/hyd/pass/fx/EntryTableRow.java
@@ -1,33 +1,57 @@
 package com.hyd.pass.fx;
 
 import com.hyd.fx.dialog.AlertDialog;
+import com.hyd.fx.system.ClipboardHelper;
 import com.hyd.pass.App;
 import com.hyd.pass.dialogs.EntryInfoDialog;
 import com.hyd.pass.model.Category;
 import com.hyd.pass.model.Entry;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableRow;
 import javafx.scene.input.MouseButton;
+import org.apache.commons.lang3.StringUtils;
 
-import static com.hyd.fx.components.MenuBuilder.contextMenu;
-import static com.hyd.fx.components.MenuBuilder.menuItem;
+import java.util.stream.Stream;
+
+import static com.hyd.fx.components.MenuBuilder.*;
 
 /**
  * @author yidin
  */
 public class EntryTableRow extends TableRow<Entry> {
 
-    private ContextMenu contextMenu = contextMenu(
-            menuItem("编辑...", this::editEntryClicked),
-            menuItem("删除", this::deleteEntryClicked)
-    );
+    private ContextMenu createContextMenu() {
+
+        String location = this.getItem().getLocation();
+        if (StringUtils.isNotBlank(location)) {
+
+            MenuItem[] locationMenuItems = Stream.of(location.split(","))
+                    .filter(StringUtils::isNotBlank)
+                    .map(l -> menuItem(l, () -> ClipboardHelper.putString(l)))
+                    .toArray(MenuItem[]::new);
+
+            return contextMenu(
+                    menuItem("编辑...", this::editEntryClicked),
+                    menuItem("删除", this::deleteEntryClicked),
+                    menu("复制地址", locationMenuItems)
+            );
+
+        } else {
+            return contextMenu(
+                    menuItem("编辑...", this::editEntryClicked),
+                    menuItem("删除", this::deleteEntryClicked)
+            );
+        }
+
+    }
 
     public EntryTableRow() {
 
         setOnContextMenuRequested(event -> {
             if (!isEmpty()) {
-                contextMenu.show(this, event.getScreenX(), event.getScreenY());
+                createContextMenu().show(this, event.getScreenX(), event.getScreenY());
             }
         });
 

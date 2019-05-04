ecd4378b687ec1b817061c580695df5a256d53a1
HeYiding <yiding.he@gmail.com>
2019-05-04 10:29:56 +0800

    更新依赖库，添加图标

--- src/main/java/com/hyd/pass/dialogs/SortCategoryChildDialog.java
+++ src/main/java/com/hyd/pass/dialogs/SortCategoryChildDialog.java
@@ -1,18 +1,17 @@
 package com.hyd.pass.dialogs;
 
 import com.hyd.fx.app.AppLogo;
-import com.hyd.fx.builders.ListViewBuilder;
+import com.hyd.fx.cells.ListCellFactory;
 import com.hyd.fx.dialog.BasicDialog;
 import com.hyd.fx.dialog.DialogBuilder;
 import com.hyd.pass.App;
 import com.hyd.pass.model.Category;
+import java.util.List;
+import java.util.function.BiFunction;
 import javafx.fxml.FXML;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TreeItem;
-
-import java.util.List;
-import java.util.function.BiFunction;
 
 /**
  * (description)
@@ -34,14 +33,14 @@ public class SortCategoryChildDialog extends BasicDialog {
         this.parent = treeItem.getValue();
 
         new DialogBuilder()
-                .title("子类排序")
-                .logo(AppLogo.getLogo())
-                .body("/fxml/sort-category-child.fxml", this)
-                .buttons(ButtonType.CLOSE)
-                .applyTo(this);
+            .title("子类排序")
+            .logo(AppLogo.getLogo())
+            .body("/fxml/sort-category-child.fxml", this)
+            .buttons(ButtonType.CLOSE)
+            .applyTo(this);
 
-        ListViewBuilder.of(lvCategories)
-                .setStringFunction(Category::getName);
+        lvCategories.setCellFactory(
+            new ListCellFactory<Category>().withTextFunction(Category::getName));
     }
 
     public void initialize() {
@@ -49,8 +48,8 @@ public class SortCategoryChildDialog extends BasicDialog {
     }
 
     public void move(
-            BiFunction<List<Category>, Integer, Boolean> canMove,
-            BiFunction<List<Category>, Integer, Integer> insertPosition
+        BiFunction<List<Category>, Integer, Boolean> canMove,
+        BiFunction<List<Category>, Integer, Integer> insertPosition
     ) {
         int selectedIndex = this.lvCategories.getSelectionModel().getSelectedIndex();
         if (selectedIndex == -1) {
@@ -71,29 +70,29 @@ public class SortCategoryChildDialog extends BasicDialog {
 
     public void moveUp() {
         move(
-                (list, selectedIndex) -> selectedIndex > 0,
-                (list, selectedIndex) -> selectedIndex - 1
+            (list, selectedIndex) -> selectedIndex > 0,
+            (list, selectedIndex) -> selectedIndex - 1
         );
     }
 
     public void moveDown() {
         move(
-                (list, selectedIndex) -> selectedIndex < list.size() - 1,
-                (list, selectedIndex) -> selectedIndex + 1
+            (list, selectedIndex) -> selectedIndex < list.size() - 1,
+            (list, selectedIndex) -> selectedIndex + 1
         );
     }
 
     public void moveTop() {
         move(
-                (list, selectedIndex) -> selectedIndex > 0,
-                (list, selectedIndex) -> 0
+            (list, selectedIndex) -> selectedIndex > 0,
+            (list, selectedIndex) -> 0
         );
     }
 
     public void moveBottom() {
         move(
-                (list, selectedIndex) -> selectedIndex < list.size() - 1,
-                (list, selectedIndex) -> list.size()
+            (list, selectedIndex) -> selectedIndex < list.size() - 1,
+            (list, selectedIndex) -> list.size()
         );
     }
 }
